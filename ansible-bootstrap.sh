2efbd620845af23e471c509937180d1d021c56b6
root <root@ip-172-31-90-250.ec2.internal>
2017-11-09 21:17:16 +0000

    aws web v.0.1

--- ansible-bootstrap.sh
+++ ansible-bootstrap.sh
@@ -22,29 +22,5 @@ git submodule update --init --recursive
 make install
 export PATH=$PATH:/usr/local/bin
 echo "export PATH=$PATH:/usr/local/bin" >> /root/.bash_profile
+source /root/.bash_profile
 
-# create aws environment
-cd /usr/local/src
-git clone https://github.com/oceanosis/awsProject
-
-##############################################################
-# Type of automation: Bootscript + Ansible + CloudFormation  #
-##############################################################
-# VARIABLES
-
-DIR=/usr/local/src/awsProject
-REGION="us-east-1"
-DATE=$(date "+%Y-%m-%d-%H-%M-%S")
-
-# Change parameters accordingly in templates
-sed -i 's/.*region:.*/region: '$REGION'/g' $DIR/ansible/roles/env/vars/main.yml
-
-export AWS_ACCESS_KEY_ID='AAAAAAAAAA'
-export AWS_SECRET_ACCESS_KEY='BBBBBBBBBB'
-
-ansible-playbook aws-automation.yml > /usr/local/src/ansible-$DATE.log
-if [[ $? -ne 0 ]]; then
-echo "Ansible playbook did not completed. Check process and errors in log"
-exit 1
-fi
-# Environment Creation is over, deployment will be done 
